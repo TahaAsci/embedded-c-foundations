9e31268cf172225650845b170bf2036a449c7854
Taha Asci <tahaasci2004@gmail.com>
2026-06-28 23:00:01 +0300

    feat: add fixed-width integer types and sizeof analysis

---
+++ 01_types_and_sizes.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdint.h>
+
+int main(){
+
+    int x;
+    uint8_t y;
+    uint16_t z;
+    uint32_t a;
+    uint64_t b;
+
+    printf("%zu bytes\n",sizeof(X));
+    printf("%zu bytes\n",sizeof(y));
+    printf("%zu bytes\n",sizeof(z));
+    printf("%zu bytes\n",sizeof(a));
+    printf("%zu bytes\n",sizeof(b));
+
+    return 0;
+}
